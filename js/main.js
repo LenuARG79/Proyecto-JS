6d8ef0a4ca594795da24c5a74b2a1a8430872aa5
LenuARG79 <lenuarg79@gmail.com>
2024-08-15 11:43:39 -0300

    V2 8

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ let inicioJuego = new Date ();
 
 const formulario = document.getElementById (`formulario`);
 const infoJugador = document.getElementById (`infoJugador`);
+const datoNombreJugador = document.getElementById (`datoNombreJugador`);
+const datoEdadJugador = document.getElementById (`datoEdadJugador`);
+const datoVitalidadJugador = document.getElementById (`datoVitalidadJugador`);
+const datoUltimoJugador = document.getElementById (`datoUltimoJugador`);
+
 let nombreJugador;
 let edad; 
 let vitalidad = 100;
@@ -13,38 +18,19 @@ formulario.addEventListener(`submit`, (e) => {e.preventDefault ();
     localStorage.setItem (`nombreJugador`, nombreJugador);
     localStorage.setItem (`edad`, edad);
     do {  
-         if (edad < 18) {
-             validarEdad.innerHTML = `Me estas mintiendo, intentalo de nuevo`;
-             document.getElementById (`edad`).value = " ";
-             document.getElementById (`edad`).focus ();
-             edad = parseInt (document.getElementById (`edad`).value);
-         } 
-     } while (edad < 18);
-    if (edad >= 18) { 
-        if (validarEdad.innerHTML.includes (`Me estas mintiendo, intentalo de nuevo`)) {
-            validarEdad.innerHTML = " ";
-        infoJugador.innerHTML = `${nombreJugador} tu fuiste seleccionado/a... Tomaras el lugar del Dr. Gordon quien ya no está entre nosotros.`
-    }};
+        if (edad < 18) {
+            validarEdad.innerHTML = `Me estas mintiendo, intentalo de nuevo`;
+            document.getElementById (`edad`).value = " ";
+            document.getElementById (`edad`).focus ();
+            edad = parseInt (document.getElementById (`edad`).value);
+        } else { 
+            if (validarEdad.innerHTML.includes (`Me estas mintiendo, intentalo de nuevo`)) {
+                validarEdad.innerHTML = " ";
+            infoJugador.innerHTML = `${nombreJugador} tu fuiste seleccionado/a... Tomaras el lugar del Dr. Gordon quien ya no está entre nosotros.`
+        }};
+    } while (edad < 18);
+infoJugador.innerHTML = `${nombreJugador} tu fuiste seleccionado/a... Tomaras el lugar del Dr. Gordon quien ya no está entre nosotros.`
 
-const datosJugador = document.getElementById (`datosJugador`);
-const datoNombreJugador = document.getElementById (`datoNombreJugador`);
-const datoEdadJugador = document.getElementById (`datoEdadJugador`);
-const datoEdadNoValido = document.getElementById (`datoEdadNoValida`);
-const datoVitalidadJugador = document.getElementById (`datoVitalidadJugador`);
-
-datoNombreJugador.innerHTML = localStorage.getItem(`nombreJugador`);
-datoEdadJugador.innerHTML = localStorage.getItem(`edad`);
-    if (edad < 18) {
-       datoEdadNoValido.innerHTML = `Edad no válida`;
-}
-datoVitalidadJugador.innerHTML = vitalidad;
-});
-
-/*{alert (`Cuentas con tu energia al ${vitalidad}% pero cada vez que pierdas en los desafios se reduciras un 25% hasta llegar a tu fin. 
-        
-Podras ver tu vitalidad cada vez que termines un desafio.`)
-}*/
-/*
 function desafiosSuperados() {
     respuestaCorrecta += 1;
 }
@@ -71,24 +57,125 @@ function nuevoJugador (nombreJugador, edad, vitalidad) {
 
 nuevoJugador (nombreJugador, edad, vitalidad);
 
-console.log (`Tu nombre es ${nombreJugador}`);
-console.log (`Tu edad es ${edad}`);
-console.log (`Tu vitalidad actual es ${vitalidad}`);
-console.log (`número de jugadores: ${jugadores.length}`);
-console.log (`Último jugador ingresado: ${jugadores[jugadores.length - 1].nombreJugador}`);
+datoNombreJugador.innerHTML = `Nombre: ${localStorage.getItem(`nombreJugador`)}`;
+datoEdadJugador.innerHTML = `Edad: ${localStorage.getItem(`edad`)}`;
+datoVitalidadJugador.innerHTML = `Vitalidad: ${vitalidad}%`;
+datoUltimoJugador.innerHTML = `Número de jugador: ${jugadores.length}`;
 
-let jugadorABuscar = prompt(`Antes de seguir... ingresa el nombre del jugador que deseas saber como terminó:
+const btnConfirmar = document.createElement (`button`);
+btnConfirmar.innerText = `Confirma tus datos`;
+document.body.appendChild (btnConfirmar);
 
-Las opciones de busqueda son: Luca, Ciro, Emma y Jime`).toUpperCase ();
+btnConfirmar.addEventListener (`click`, () => {
+Swal.fire({
+    title: '¿Estás seguro de que tus datos son correctos?',
+    text: `Nombre: ${nombreJugador}, Edad: ${edad}`,
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonText: 'Sí, estoy seguro',
+    cancelButtonText: 'No, revisar datos'
+    }).then((result) => {
+    if (result.isConfirmed) {
+        Swal.fire({
+        title: 'Vitalidad',
+        text: `Cuentas con tu energía al ${vitalidad}% pero cada vez que pierdas en los desafíos se reducirás un 25% hasta llegar a tu fin. 
+
+        Podrás ver tu vitalidad cada vez que termines un desafío.`,
+        icon: 'info',
+        confirmButtonText: 'Entendido'
+    });
+    const formularioJugador = document.createElement (`form`);
+    const formularioJugadorNombre = document.createElement (`label`);
+    formularioJugadorNombre.textContent = `Ingresa el nombre del jugador que ha intentado escapar antes que vos`;
+    const formularioJugadorSelect = document.createElement (`select`);
+
+    jugadores.forEach((jugador, i) => {
+    if (i < jugadores.length - 1) {
+    const opcion = document.createElement("option");
+    opcion.value = jugador.nombreJugador;
+    opcion.textContent = jugador.nombreJugador;
+    formularioJugadorSelect.appendChild(opcion);
+}});
+formularioJugadorNombre.appendChild (formularioJugadorSelect);
+formularioJugador.appendChild (formularioJugadorNombre);
+document.body.appendChild (formularioJugador);
+
+    formularioJugadorSelect.addEventListener('change', (e) => {
+    const jugadorSeleccionado = jugadores.find(j => j.nombreJugador === e.target.value);
+    const infoJugadorSeleccionado = document.createElement('div');
+    infoJugadorSeleccionado.innerHTML = `
+    <p>Nombre: ${jugadorSeleccionado.nombreJugador}</p>
+    <p>Edad: ${jugadorSeleccionado.edad}</p>
+    <p>Vitalidad: ${jugadorSeleccionado.vitalidad}%</p>
+    <p>Muerte por falta de tiempo para escapar</p>`;
+
+    formularioJugador.appendChild(infoJugadorSeleccionado);
+
+    const botonContinuar = document.createElement('button');
+    botonContinuar.innerText = 'Continuar';
+    formularioJugador.appendChild(botonContinuar);
+
+    botonContinuar.addEventListener('click', () => { 
+    const trampasAElegir = document.createElement (`div`);
+    trampasAElegir.innerText = `Estas son todas las trampas que tendras que superar si queres salir con vida o algo de ella. 
+        - Trampa en el laberinto de espinas. 
+        - Trampa de la cera y la vela.
+        - Trampa para osos invertida.
+        - Trampa del cuarto de baño.`;
     
-let jugadorBuscado = jugadores.find(j => j.nombreJugador === jugadorABuscar);
-    alert (`Jugador encontrado:
-Nombre: ${jugadorBuscado.nombreJugador};
-Edad: ${jugadorBuscado.edad};
-Vitalidad: ${jugadorBuscado.vitalidad + "%"};
-Muerte por falta de tiempo para escapar.`);
+    document.body.appendChild (trampasAElegir);    
     
-let opcion = prompt (`Estas son todas las trampas que tendras que superar si queres salir con vida o algo de ella. Ingresa el número "1" para avanzar:
+    const botonTrampa1 = document.createElement('button');
+    botonTrampa1.innerText = 'Trampa 1';
+    trampasAElegir.appendChild(botonTrampa1);
+    
+    botonTrampa1.addEventListener('click', () => {
+        Swal.fire({
+        title: 'Trampa 1',
+        text: 'Aqui te va el desafío. Para pasar a la proxima trampa deberas acertar',
+        icon: 'info',
+        confirmButtonText: 'Entendido'
+})
+    trampasAElegir.appendChild (botonTrampa1);
+
+    let ingresoPrimerDesafio = parseInt (prompt (`Tres números tengo, tres.
+Si no acertas que número soy, 
+corriendo por ti voy.`));
+        if (ingresoPrimerDesafio === 333) {
+        alert (`Tu vitalidad sigue siendo del ${vitalidad}%.
+Continua con el siguiente desafio`);
+        jugadores.push ({nombreJugador, edad, vitalidad}); 
+        jugadores.vitalidad = vitalidad;
+        console.log ("Tu vitaliodad es: " + jugadores.vitalidad + "%");
+        desafiosSuperados ();    
+        alert (`Desafios superados: ${respuestaCorrecta}`);
+        } else if (ingresoPrimerDesafio != 333) {
+        vitalidad = vitalidad - 25;
+        alert (`Tu vitalidad se redujo al ${vitalidad}%. 
+Continua con el siguiente desafio`);
+        jugadores.push ({nombreJugador, edad, vitalidad});
+        jugadores.vitalidad = vitalidad;
+        console.log (`Desafios superados: ${respuestaCorrecta}`);
+        console.log ("Tu vitaliodad es: " + jugadores.vitalidad + "%");
+}
+});    
+})
+});
+    } else {
+        Swal.fire({
+        title: 'Revisar datos',
+        text: 'Por favor, revisa tus datos y vuelve a intentarlo.',
+        icon: 'error',
+        confirmButtonText: 'Entendido'
+    });
+}
+});
+});
+
+
+});
+
+/*    let opcion = prompt (`Estas son todas las trampas que tendras que superar si queres salir con vida o algo de ella. Ingresa el número "1" para avanzar:
 
     1. Trampa en el laberinto de espinas.
     -  Trampa de la cera y la vela.
@@ -210,7 +297,7 @@ Nos vemos en la próxima Pre-Entrega`);
         alert ("HASTA LA PROXIMA PRE-ENTREGA");
     }
     break;
-    }
+    }*/
     let finJuego = new Date;
     let tiempoDeJuego = finJuego - inicioJuego;
-    alert (`Tu juego a durado: ${tiempoDeJuego} milisegundos`);*/
+    alert (`Tu juego a durado: ${tiempoDeJuego} milisegundos`);
